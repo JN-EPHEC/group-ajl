670e3be5929bdd31b181fe7f94e13b56268fbd44
Lucas-schnkl <lucas.schenkel12@gmail.com>
2026-05-10 15:45:00 +0200

    Changement fichier de la db pour que fonctionne

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,31 +1,37 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url"; // Import requis pour recréer __dirname
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
 
-// 1. Recréer __dirname et __filename pour le scope ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-// 2. Charger le fichier .env à la racine du projet
-// Remarque : Si votre fichier s'appelle ".env.production", remplacez ".env" par ".env.production" ci-dessous
-dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
+class Database {
+  private static instance: Sequelize;
+  
+  private constructor() {
+  // Constructeur prive pour empecher l'instanciation directe avec 'new'
+  }
+  
+  public static getInstance(): Sequelize {
+    if (!Database.instance) {
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error("La variable d'environnement DATABASE_URL n'est pas définie.");
+      Database.instance = new Sequelize({
+        username: process.env.DB_USER!,
+        password: process.env.DB_PASSWORD!,
+        database: "postgres",
+        host: process.env.DB_HOST!,
+        port: Number(process.env.DB_PORT!),
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: {
+          require: true,
+          rejectUnauthorized: false,
+          },
+        },
+      logging: false,
+    });
+  }
+    return Database.instance;
+  }
 }
 
-const sequelize = new Sequelize(databaseUrl, {
-  dialect: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Requis pour Supabase
-    },
-  } as any,
-});
 
-export default sequelize;
+export default Database.getInstance();
