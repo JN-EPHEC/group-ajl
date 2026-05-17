8bedc9e315f6c61c8a371ca6d7b4021f920d64ec
Lucas-schnkl <lucas.schenkel12@gmail.com>
2026-05-17 22:54:15 +0200

    Ajout logo tmdb

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,30 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+
+      {/* Logo TMDB */}
+      <a
+        href="https://www.themoviedb.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          position: 'fixed',
+          bottom: '15px',
+          right: '15px',
+          zIndex: 9999,
+        }}
+      >
+        <img
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+          alt="TMDB"
+          style={{
+            width: '90px',
+            opacity: 0.8,
+          }}
+        />
+      </a>
+    </>
   </StrictMode>,
 )
