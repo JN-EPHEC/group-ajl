933d93b1979c71d0080249392dc683f54ccf724d
Caribouspatial <ju.aerts@students.ephec.be>
2026-05-27 10:27:25 +0200

    fix: tsconfig.json instalation des modules bcrypt et jsonwebtoken et modification de compatibilité .js et .ts dans jest.config.js
    Signed-off-by: Caribouspatial <ju.aerts@students.ephec.be>

--- server/jest.config.js
+++ server/jest.config.js
@@ -9,5 +9,18 @@ export default {
   transform: {
     ...tsJestTransformCfg,
   },
+  // Résout les imports ESM avec extension .js vers .ts (nécessaire, car le code source
+  // utilise des imports avec .js pour la compatibilité ESM/Node)
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  // Pointe vers ce dossier test_unitaires
   testMatch: ['**/src/tests/**/*.test.ts'],
+  //quel fichier mesurer
+  collectCoverageFrom: [
+    "../server/src/utils/**/*.ts",
+    "../server/src/middlewares/**/*.ts",
+    "../server/src/controllers/**/*.ts",
+    "!../server/src/**/*.d.ts",
+  ],
 };
